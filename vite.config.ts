29a3d534c05020d4328c6372637ac5e7d24efa50
richisil2005 <sanmigue2005@gmail.com>
2026-06-20 11:28:43 -0500

    3.0

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/n/axz4ksbn6fim/b/xoc-frontend/o/',
+  base: '/',
   build: {
     assetsDir: ''
   },
